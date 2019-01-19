378030f4f62d9c2d3ffcbbd39e09224ec91d211a
Mike Centola <mcentola@appliedengdesign.com>
2019-01-19 18:06:56 -0500

    Removed Colorization Attempt to push to dev branch

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,73 @@
 import * as vscode from 'vscode';
+//import * as util from './util';
+//import { getColorization } from './colorization';
 
 export function activate(context: vscode.ExtensionContext) {
-    
+    /*
     // Get Configuration
-    let gcodeConf = vscode.workspace.getConfiguration();
+    let gcodeconf: vscode.WorkspaceConfiguration = util.getConfig();
 
-    console.log('gcode.colorization:' + gcodeConf.get('gcode.colorization'));
+
+    if (gcodeconf.colorization) {
+
+        // Gcode Configuration is on
+
+        console.log('gcode.colorization:' + gcodeconf.colorization);
+
+    }
+
+    
+
 
     context.subscriptions.push(
         vscode.workspace.onDidChangeConfiguration(e => {
 
             if (e.affectsConfiguration('gcode.colorization')) {
-             
-                let tokenColors = vscode.workspace.getConfiguration('editor.tokenColorCustomizations');
 
-                console.log(tokenColors.textMateRules);
-                
+                // Get Configuration
+                let gcodeconf: vscode.WorkspaceConfiguration = util.getConfig();
+             
+                console.log('gcode.colorization:' + gcodeconf.colorization);
+            
                 if (vscode.workspace.getConfiguration().get('gcode.colorization')) {
                     console.log('toggle on');
+
+                    let colors = getColorization();
+
+                    if (vscode.workspace.getConfiguration().has('editor.tokenColorCustomizations')) {
+                        let tokenColors = vscode.workspace.getConfiguration('editor.tokenColorCustomization');
+                        console.log('Storing User Colorization');
+                        let userColors = util.storeUserTokenColorCustomizations(tokenColors);
+                    }
+
+                    console.log('No textMateRules. Adding colorization\n' + colors)
+                    
+                    // textMateRules doesn't exist so add it
+                    vscode.workspace.getConfiguration().update(
+                        'editor.tokenColorCustomizations',
+                        {
+                            "textMateRules": colors
+                        },
+                        vscode.ConfigurationTarget.Global
+                    );
+                    console.log('Added.');
+                    console.log(vscode.workspace.getConfiguration('editor.tokenColorCustomization').has('textMateRules'));
+
                 } else {
                     console.log('toggle off');
+                    vscode.workspace.getConfiguration().update(
+                        'editor.tokenColorCustomizations',
+                        undefined,
+                        vscode.ConfigurationTarget.Global
+                    );
+                    console.log('Removed');
+                    console.log(vscode.workspace.getConfiguration('editor.tokenColorCustomization').has('textMateRules'));
                 }
             }
 
-    }));
+    }));*/
+}
+
+export function deactivate() {
+
 }
