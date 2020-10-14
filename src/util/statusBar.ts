49149ddf53f7d67c68db072040853d033151a817
Mike Centola <mcentola@appliedengdesign.com>
2020-10-14 19:58:45 -0400

    Added statusbar functionality
    Signed-off-by: Mike Centola <mcentola@appliedengdesign.com>

---
+++ src/util/statusBar.ts
@@ -0,0 +1,43 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Applied Eng & Design All rights reserved.
+ *  Licensed under the MIT License. See License.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+
+import { ExtensionContext, StatusBarAlignment, StatusBarItem, window } from "vscode";
+
+
+export class StatusBar {
+    static statusBar: StatusBarItem
+
+    static configure(context: ExtensionContext) {
+        this.statusBar = this.statusBar || window.createStatusBarItem( StatusBarAlignment.Right, 100 );
+
+        context.subscriptions.push(this.statusBar);
+    }
+
+    static updateStatusBar(message: string): void {
+        if (this.statusBar !== undefined) {
+            this.statusBar.text = message;
+            this.statusBar.show();
+        }
+    }
+
+    static showStatusBar (): void {
+        if (this.statusBar !== undefined) {
+            this.statusBar.show();
+        }
+    }
+
+    static hideStatusBar(): void {
+        if (this.statusBar !== undefined) {
+            this.statusBar.hide();
+        }
+    }
+
+    static dispose(): void {
+        if (this.statusBar !== undefined) {
+            this.statusBar.dispose();
+        }
+    }
+}
