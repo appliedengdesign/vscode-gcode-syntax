1485f55843e91b9053db0c5e2766bdb2341b3c3f
Mike Centola <mcentola@appliedengdesign.com>
2021-02-06 18:59:30 -0500

    Cleaned up main extension activation

--- src/extension.ts
+++ src/extension.ts
@@ -4,19 +4,14 @@
  *--------------------------------------------------------------------------------------------*/
 
 'use strict';
-import { commands, ExtensionContext, window } from 'vscode';
+import { ExtensionContext } from 'vscode';
 import { Config, configuration } from './util/config';
 import { constants } from './util/constants';
 import { Logger } from './util/logger';
 import { StatusBar } from './util/statusBar';
-//import { GCodeTreeView } from './views/gcodeTreeView';
 import { GCommand } from './util/commands';
-import { config } from 'process';
 import { Control } from './control';
 
-
-//import { GCodeHoverProvider } from './providers/gcodeHover';
-//import { getColorization } from './colorization';
 
 
 export async function activate(context: ExtensionContext) {
@@ -36,78 +31,13 @@ export async function activate(context: ExtensionContext) {
     Logger.log(constants.extension.shortname + " v" + constants.extension.version + " activated.");
     Logger.log(constants.copyright);
 
+    // Register Commands
     GCommand.registerCommands(context);
     
-
+    // Initialize Controller
     Control.initialize(context, configuration);
     
-
-    /*
-    context.subscriptions.push(
-        vscode.languages.registerHoverProvider(
-            GCODE, new GCodeHoverProvider()
-        )
-    );
-
-
-    if (gcodeconf.colorization) {
-
-        // Gcode Configuration is on
-
-        console.log('gcode.colorization:' + gcodeconf.colorization);
-
-    }
-
     
-
-
-    context.subscriptions.push(
-        vscode.workspace.onDidChangeConfiguration(e => {
-
-            if (e.affectsConfiguration('gcode.colorization')) {
-
-                // Get Configuration
-                let gcodeconf: vscode.WorkspaceConfiguration = util.getConfig();
-             
-                console.log('gcode.colorization:' + gcodeconf.colorization);
-            
-                if (vscode.workspace.getConfiguration().get('gcode.colorization')) {
-                    console.log('toggle on');
-
-                    let colors = getColorization();
-
-                    if (vscode.workspace.getConfiguration().has('editor.tokenColorCustomizations')) {
-                        let tokenColors = vscode.workspace.getConfiguration('editor.tokenColorCustomization');
-                        console.log('Storing User Colorization');
-                        let userColors = util.storeUserTokenColorCustomizations(tokenColors);
-                    }
-
-                    console.log('No textMateRules. Adding colorization\n' + colors)
-                    
-                    // textMateRules doesn't exist so add it
-                    vscode.workspace.getConfiguration().update(
-                        'editor.tokenColorCustomizations',
-                        {
-                            "textMateRules": colors
-                        },
-                        vscode.ConfigurationTarget.Global
-                    );
-                    console.log('Added.');
-                    console.log(vscode.workspace.getConfiguration('editor.tokenColorCustomization').has('textMateRules'));
-
-                } else {
-                    console.log('toggle off');
-                    vscode.workspace.getConfiguration().update(
-                        'editor.tokenColorCustomizations',
-                        undefined,
-                        vscode.ConfigurationTarget.Global
-                    );
-                    console.log('Removed');
-                    console.log(vscode.workspace.getConfiguration('editor.tokenColorCustomization').has('textMateRules'));
-                }
-            }
-
-    }));*/
 }
 
 export function deactivate() {
