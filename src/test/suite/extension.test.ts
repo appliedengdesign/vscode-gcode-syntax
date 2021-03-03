8c9601c7f3d44639810a6502848b94548da7a806
Mike Centola <mcentola@appliedengdesign.com>
2021-03-03 12:53:53 -0500

    work on testing

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,17 +3,24 @@
  *  Licensed under the MIT License. See License.md in the project root for license information.
  * -------------------------------------------------------------------------------------------- */
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
+import { after } from 'mocha';
 import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
 
 suite('Extension Test Suite', () => {
     void vscode.window.showInformationMessage('Start all tests.');
+
+    after(() => {
+        void vscode.window.showInformationMessage('All Tests Done!');
+    });
 
     test('Sample test', () => {
         assert.strictEqual(-1, [1, 2, 3].indexOf(5));
         assert.strictEqual(-1, [1, 2, 3].indexOf(0));
     });
 });
+
+function sleep(ms: number): Promise<void> {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+}
