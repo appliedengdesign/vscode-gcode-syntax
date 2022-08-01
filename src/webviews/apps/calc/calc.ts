c1e25db625141532bea445525e2b4debaab98681
Mike Centola <mcentola@appliedengdesign.com>
2022-08-01 17:33:46 -0400

    Fixed finding input box within shadowRoot

--- src/webviews/apps/calc/calc.ts
+++ src/webviews/apps/calc/calc.ts
@@ -13,15 +13,16 @@ interface RPMCalc {
 
 export class CalcApp {
     private _rpmCalc: RPMCalc;
+
     constructor() {
         this._rpmCalc = {
             btn: document.getElementById('rpm-calc')!,
-            sfm: document.getElementById('sfm') as HTMLInputElement,
-            toolDia: document.getElementById('tool-dia') as HTMLInputElement,
-            results: document.getElementById('rpm-results') as HTMLInputElement,
+            sfm: document.getElementById('sfm')?.shadowRoot?.getElementById('control') as HTMLInputElement,
+            toolDia: document.getElementById('tool-dia')?.shadowRoot?.getElementById('control') as HTMLInputElement,
+            results: document.getElementById('rpm-results')?.shadowRoot?.getElementById('control') as HTMLInputElement,
         };
 
-        this._rpmCalc.btn.addEventListener('click', this.calcRPM);
+        this._rpmCalc.btn.addEventListener('click', this.calcRPM.bind(this), false);
     }
 
     private calcRPM(): void {
