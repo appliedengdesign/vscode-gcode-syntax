eb28e66e79fa992283705d8913c6fd9e26e97ad7
Mike Centola <mcentola@appliedengdesign.com>
2022-09-21 20:46:21 -0400

    Shortened regex line

--- src/views/providers/helpers.ts
+++ src/views/providers/helpers.ts
@@ -5,10 +5,5 @@
 'use strict';
 
 export function stripComments(line: string): string {
-    // eslint-disable-next-line
-    const re1 = /\s*\([^\)]*\)/g; // Remove anything inside the parentheses
-    const re2 = /\s*;.*/g; // Remove anything after a semi-colon to the end of the line, including preceding spaces
-    const re3 = /\s+/g;
-
-    return line.replace(re1, '').replace(re2, '').replace(re3, '');
+    return line.replace(/\s*\(.*\)\s*$|^\s*;.*/g, '');
 }
